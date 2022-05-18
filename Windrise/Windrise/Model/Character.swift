dda39920c5a47187f5fe120827278d5b22e61ee3
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-18 20:44:36 +0700

    character model from json

---
+++ Windrise/Windrise/Model/Character.swift
@@ -0,0 +1,51 @@
+//
+//  Character.swift
+//  Windrise
+//
+//  Created by MacBook Pro on 18/05/22.
+//
+
+import Foundation
+
+// MARK: - Character
+struct Character: Codable, Identifiable {
+    let id = UUID()
+    var name: String = ""
+    var vision: String = ""
+    var weapon: String = ""
+    var nation: String = ""
+    var affiliation: String = ""
+    var rarity: Int=0
+    var constellation: String = ""
+    var birthday: String = ""
+    var welcomeDescription: String = ""
+    var skillTalents: String = ""
+    var passiveTalents: String = ""
+    var constellations: [Constellation] = []
+    var visionKey: String = ""
+    var weaponType: String = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case name, vision, weapon, nation, affiliation, rarity, constellation, birthday
+        case welcomeDescription = "description"
+        case skillTalents, passiveTalents, constellations
+        case visionKey = "vision_key"
+        case weaponType = "weapon_type"
+    }
+}
+
+// MARK: - Constellation
+struct Constellation: Codable, Identifiable {
+    let id = UUID()
+    var name: String = ""
+    var unlock: String = ""
+    var constellationDescription: String = ""
+    var level: Int? = 0
+    var type: String? = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case name, unlock
+        case constellationDescription = "description"
+        case level, type
+    }
+}
