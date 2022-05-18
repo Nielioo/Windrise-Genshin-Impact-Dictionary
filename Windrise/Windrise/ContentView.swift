e210105beac20c98379738f41d5e4cbfa69088e0
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-18 19:50:51 +0700

    first init

---
+++ Windrise/Windrise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Windrise
+//
+//  Created by MacBook Pro on 18/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Windrise/Windrise/WindriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindriseApp.swift
+//  Windrise
+//
+//  Created by MacBook Pro on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WindriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
