a15492d4d945c711b7a67f50fb882e76389c30ce
Daniel Aprillio <danielaprillio02@gmail.com>
2022-05-19 21:39:06 +0700

    coloring progress

--- Windrise/Windrise/View/Character/CharacterDescription.swift
+++ Windrise/Windrise/View/Character/CharacterDescription.swift
@@ -12,65 +12,79 @@ struct CharacterDescription: View {
     @State var currentCharacter: Character
     @State var currentCharacterGachaSplash: Image = Image("characterDummy")
     
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                VStack{
-                    currentCharacterGachaSplash
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                
-                VStack{
-                    Text(currentCharacter.name)
-                    Text(currentCharacter.rarity.description)
-                    Text(currentCharacter.vision)
-                    Text(currentCharacter.weapon)
-                    Text(currentCharacter.birthday)
-                    Text(currentCharacter.nation)
-                    Text(currentCharacter.constellation)
-                }
-                
-                VStack{
-                    Text(currentCharacter.description)
-                }
-                
-                
-                VStack{
-                    ForEach(currentCharacter.skillTalents){ skill in
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color("BackgroundDarkBlue"), Color("BackgroundDarkPurple")]), startPoint: .topLeading, endPoint: .bottomLeading)
+                .ignoresSafeArea()
+            ScrollView {
+                VStack(alignment: .center) {
+                    ZStack(alignment: .bottomTrailing){
+                        // MARK: - Gacha Splash
                         VStack{
-                            Text(skill.name)
-                            Text(skill.description)
-                            //Text(skill.type ?? "")
+                            currentCharacterGachaSplash
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }.ignoresSafeArea()
+                        // MARK: - Character Intro
+                        VStack{
+                            Text(currentCharacter.name).padding(.top, 12)
+                            Text(currentCharacter.rarity.description)
+                            Text(currentCharacter.vision)
+                            Text(currentCharacter.weapon)
+                            Text(currentCharacter.birthday)
+                            Text(currentCharacter.nation)
+                            Text(currentCharacter.constellation).padding(.bottom, 12)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(Color.black.opacity(0.7))
+                        .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    VStack{
+                        Text(currentCharacter.description)
+                    }
+                    
+                    
+                    VStack{
+                        ForEach(currentCharacter.skillTalents){ skill in
+                            VStack{
+                                Text(skill.name)
+                                Text(skill.description)
+                                //Text(skill.type ?? "")
+                            }
                         }
                     }
-                }
-                
-                VStack{
-                    ForEach(currentCharacter.passiveTalents){ passive in
-                        VStack{
-                            Text(passive.name)
-                            Text(passive.description)
+                    
+                    VStack{
+                        ForEach(currentCharacter.passiveTalents){ passive in
+                            VStack{
+                                Text(passive.name)
+                                Text(passive.description)
+                            }
                         }
                     }
-                }
-                
-                VStack{
-                    ForEach(currentCharacter.constellations){ cons in
-                        Text(cons.level?.description ?? "0")
-                        Text(cons.name)
-                        Text(cons.description)
+                    
+                    VStack{
+                        ForEach(currentCharacter.constellations){ cons in
+                            Text(cons.level?.description ?? "0")
+                            Text(cons.name)
+                            Text(cons.description)
+                        }
                     }
+                    
                 }
-                
-            }
-            .onAppear{
-                Api().getCharacter(currentCharacterName: currentCharacter.name) { character in
-                    self.currentCharacter = character
-                }
-                
-                Api().getCharacterGachaSplash(currentCharacterName: currentCharacter.name) { image in
-                    self.currentCharacterGachaSplash = image
+                .padding(.horizontal, 12.0)
+                // MARK: - Api Settings
+                .onAppear{
+                    Api().getCharacter(currentCharacterName: currentCharacter.name) { character in
+                        self.currentCharacter = character
+                    }
+                    
+                    Api().getCharacterGachaSplash(currentCharacterName: currentCharacter.name) { image in
+                        self.currentCharacterGachaSplash = image
+                    }
                 }
             }
         }
