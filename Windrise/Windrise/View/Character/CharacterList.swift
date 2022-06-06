796728e918864afa68248cc261e28f64c5bf7c5b
Daniel Aprillio <danielaprillio02@gmail.com>
2022-06-06 19:31:16 +0700

    search character done

--- Windrise/Windrise/View/Character/CharacterList.swift
+++ Windrise/Windrise/View/Character/CharacterList.swift
@@ -13,6 +13,17 @@ struct CharacterList: View {
     
     private var columns : [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     
+    @State private var searchText = ""
+    var searchResults: [Character] {
+        if(searchText.isEmpty){
+            return mainViewModel.characters
+        }else{
+            return mainViewModel.characters.filter{
+                $0.name.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
+    
     var body: some View {
         
         NavigationView {
@@ -20,27 +31,68 @@ struct CharacterList: View {
                 MyColor.mainBackground.ignoresSafeArea()
                 
                 ScrollView(showsIndicators: false){
+                    
+                    TextField("Search Characters", text: $searchText)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.white, lineWidth: 1.5)
+                        )
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                        .padding(.top, 40)
+                        .disableAutocorrection(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
+                    
                     LazyVGrid(columns: columns){
+                        
                         ForEach(mainViewModel.characters.indices, id: \.self){ index in
-                            NavigationLink(
-                                destination:
-                                    CharacterDescription(
+                            
+                            if(!searchResults.isEmpty){
+                                
+                                ForEach(searchResults){ filteredCharacter in
+                                    
+                                    if(mainViewModel.characters[index].name.lowercased()
+                                        .contains(filteredCharacter.name.lowercased())){
+                                        NavigationLink(
+                                            destination:
+                                                CharacterDescription(
+                                                    currentNameId: mainViewModel.nameId[index],
+                                                    currentCharacter: mainViewModel.characters[index]))
+                                        {
+                                            CharacterCard(
+                                                currentNameId: mainViewModel.nameId[index],
+                                                currentCharacter: mainViewModel.characters[index])
+                                        }
+                                        .tag(mainViewModel.characters[index].name)
+                                        .buttonStyle(PlainButtonStyle())
+                                    }
+                                    
+                                }
+                                
+                            } else {
+                                NavigationLink(
+                                    destination:
+                                        CharacterDescription(
+                                            currentNameId: mainViewModel.nameId[index],
+                                            currentCharacter: mainViewModel.characters[index]))
+                                {
+                                    CharacterCard(
                                         currentNameId: mainViewModel.nameId[index],
-                                        currentCharacter: mainViewModel.characters[index]))
-                            {
-                                CharacterCard(
-                                    currentNameId: mainViewModel.nameId[index],
-                                    currentCharacter: mainViewModel.characters[index])
+                                        currentCharacter: mainViewModel.characters[index])
+                                }
+                                .tag(mainViewModel.characters[index].name)
+                                .buttonStyle(PlainButtonStyle())
                             }
-                            .tag(mainViewModel.characters[index].name)
-                            .buttonStyle(PlainButtonStyle())
+                            
                         }
                     }
                     .padding()
                 }
                 .foregroundColor(.white)
             }
-            .navigationTitle(/*@START_MENU_TOKEN@*/"Character List"/*@END_MENU_TOKEN@*/)
+//            .navigationTitle(/*@START_MENU_TOKEN@*/"Character List"/*@END_MENU_TOKEN@*/)
+            .navigationBarHidden(true)
         }
         .onAppear {
             UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
